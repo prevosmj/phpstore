378065c3382d60e22c6c8b840e355ff1d473af5b
prevosmj <69904719+prevosmj@users.noreply.github.com>
2022-04-16 14:42:54 -0400

    Update database.php

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $db_name = "php_beginner_crud_level_1";
 $username = "phpstore";
-$password = "sorryivemissedclass";
+$password = "strongpasswordexample";
  
 try {
     $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
